72c582dc35cd6694dd2b73d25093b94532494f9c
tal-diamant <erkeliwood@gmail.com>
2023-07-26 12:17:55 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	let categories = ['Dairy', 'Vegetables', 'Chocolate', 'Frozen', 'Meats'];
+</script>
+
+<main>
+	<h1>Shopping List</h1>
+	<div>
+		<h2>Categories</h2>
+		{#each categories as category}
+			<h3>{category}</h3>
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		font-family: sans-serif;
+	}
+</style>
